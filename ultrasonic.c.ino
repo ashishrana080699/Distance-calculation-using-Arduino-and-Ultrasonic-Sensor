4f28248670ca7fd33c38b7368fa1a50bb97ee7e5
Ashish Rana <aashishrana269@gmail.com>
2020-04-02 12:18:50 +0530

    Update ultrasonic.c.ino

--- ultrasonic.c.ino
+++ ultrasonic.c.ino
@@ -7,8 +7,8 @@ long duration;
 int distance;
 
 void setup() {
-pinMode(trigPin, OUTPUT); //  Sets the trigPin as an Output
-pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+pinMode(echoPin, INPUT); //Sets the echoPin as an Input
 Serial.begin(9600); // Starts the serial communication
 }
 
